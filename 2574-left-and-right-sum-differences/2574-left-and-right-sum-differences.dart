af0d22603a9971bfbb2a0cc52a248d2c70334e25
rayisrayis01-tech <rayisrayis01@gmail.com>
2026-06-06 11:50:13 +0530

    Time: 2 ms (60%), Space: 151.2 MB (40%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.dart
@@ -0,0 +1,17 @@
+class Solution {
+  List<int> leftRightDifference(List<int> nums) {
+  int n = nums.length;
+  int a = nums.reduce((a, b) => a + b);
+
+  List<int> b = List.filled(n, 0);
+  int c = 0;
+
+  for (int i = 0; i < n; i++) {
+    int d = a - c - nums[i];
+    b[i] = (c - d).abs();
+    c += nums[i];
+  }
+
+  return b;
+  }
+}
